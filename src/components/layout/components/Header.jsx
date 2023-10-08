304ed64f883f735b954306aa86f77cd69b623f72
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-10-08 19:50:34 +0200

    add: amount display to cart

--- src/components/layout/components/Header.jsx
+++ src/components/layout/components/Header.jsx
@@ -7,8 +7,11 @@ export default function Header() {
       <div className="text-xl font-extrabold bg-green-300 p-2 m-2">
         <Link to="/"> Logo</Link>
       </div>
-      <div className="p-2 m-2">
+      <div className="p-2 m-2 relative">
         <CartIcon />
+        <div className="container w-4 bg-red-600 rounded-full text-center font-bold text-xs absolute right-0 top-0">
+          1
+        </div>
       </div>
     </header>
   );
