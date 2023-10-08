e5161b30d920e6ccff8fe0d013f00750717a149e
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-10-08 19:31:32 +0200

    add: cart icon to Header

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 
 export default function Layout() {
   return (
     <>
       <Header />
+      <Nav />
       <Outlet />
       <Footer />
     </>
--- src/components/layout/components/Header.jsx
+++ src/components/layout/components/Header.jsx
@@ -1,10 +1,15 @@
-import Nav from "./Nav";
+import { Link } from "react-router-dom";
+import { CartIcon } from "./icons/cart";
 
 export default function Header() {
   return (
-    <header className="bg-sky-950">
-      <div>Header with Logo and nav</div>
-      <Nav />
+    <header className="flex justify-between bg-sky-950 p-3">
+      <div className="text-xl font-extrabold bg-green-300 p-2 m-2">
+        <Link to="/"> Logo</Link>
+      </div>
+      <div className="p-2 m-2">
+        <CartIcon />
+      </div>
     </header>
   );
 }
