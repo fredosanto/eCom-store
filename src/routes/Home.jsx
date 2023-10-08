0729c513b2f2f2865761b136c51297fb89871b04
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-10-08 19:30:22 +0200

    add: fetch all products from api

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,36 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const url = "https://api.noroff.dev/api/v1/online-shop";
+
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(url);
+      const json = await response.json();
+
+      setProducts(json);
+    }
+    getData();
+  }, []);
+
   return (
     <>
-      <h1 className="text-sky-400">Home</h1>
+      <h1 className="text-black text-4xl font-bold my-6">Home</h1>
+      <h2 className="text-black text-2xl">Products</h2>
+      <div className="grid grid-cols-4 gap-5 p-2 text-black  ">
+        {products.map((product) => (
+          <div key={product.id} className="text-center bg-sky-300 p-5">
+            <h3>{product.title}</h3>
+            <p>{product.price}</p>
+            <div>
+              <button className="bg-emerald-500 px-5 py-1 mx-2">+</button>
+              <button className="bg-red-500 px-5 py-1 mx-2">-</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
