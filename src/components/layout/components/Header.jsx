673ecea48b76995ce805ed4889bd99272ccf14fc
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-10-09 15:46:52 +0200

    add: state to cart icon

--- src/components/layout/components/Header.jsx
+++ src/components/layout/components/Header.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
 import { CartIcon } from "./icons/cart";
+import { useState } from "react";
 
 export default function Header() {
+  const [inCartCount, setInCartCount] = useState(0);
   return (
     <header className="flex justify-between bg-sky-950 p-3">
       <div className="text-xl font-extrabold bg-green-300 p-2 m-2">
@@ -10,7 +12,7 @@ export default function Header() {
       <div className="p-2 m-2 relative">
         <CartIcon />
         <div className="container w-4 bg-red-600 rounded-full text-center font-bold text-xs absolute right-0 top-0">
-          1
+          {inCartCount}
         </div>
       </div>
     </header>
