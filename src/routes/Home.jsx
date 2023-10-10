be5c1a9ff2b70ba2c7d4223ab4f424c8c45aae79
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-10-10 13:26:52 +0200

    add: search function to render by filter

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,32 +5,43 @@ const url = "https://api.noroff.dev/api/v1/online-shop";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
+  const [searchContent, setSearchContent] = useState("");
 
   useEffect(() => {
     async function getData() {
       const response = await fetch(url);
       const json = await response.json();
 
-      console.log(json);
       setProducts(json);
+      console.log(json);
     }
     getData();
   }, []);
 
+  const searchItems = products.filter((item) =>
+    item.title.toLowerCase().includes(searchContent.toLowerCase())
+  );
+
   return (
     <>
       <h1 className="text-black text-4xl font-bold my-6">Home</h1>
-      <h2 className="text-black text-2xl">Products</h2>
-      <input type="text" />
+      <h2 className="text-black text-2xl">All Products</h2>
+      <input
+        className="text-black"
+        value={searchContent}
+        onChange={(event) => {
+          setSearchContent(event.target.value);
+        }}
+      />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-5 p-2 text-black  ">
-        {products.map((product) => (
+        {searchItems.map((item) => (
           <Item
-            key={product.id}
-            id={product.id}
-            title={product.title}
-            price={product.price}
-            discount={product.discountedPrice}
-            img={product.imageUrl}
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            discount={item.discountedPrice}
+            img={item.imageUrl}
           />
         ))}
       </div>
