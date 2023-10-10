1eb5844e95a6247f63eb2916e464386c61db05bc
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-10-10 20:43:28 +0200

    add: adjustment to search field

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -24,15 +24,18 @@ export default function Home() {
 
   return (
     <>
-      <h1 className="text-black text-4xl font-bold my-6">Home</h1>
-      <h2 className="text-black text-2xl">All Products</h2>
-      <input
-        className="text-black"
-        value={searchContent}
-        onChange={(event) => {
-          setSearchContent(event.target.value);
-        }}
-      />
+      <h1 className="text-black text-4xl font-bold m-4">Home</h1>
+      <h2 className="text-black text-2xl m-4">All Products</h2>
+      <div className="container m-4">
+        <input
+          className="text-black bg-slate-100 p-2 border-2 border-slate-400 shadow-md rounded-md focus:outline-none focus:shadow-inner"
+          placeholder="Search..."
+          value={searchContent}
+          onChange={(event) => {
+            setSearchContent(event.target.value);
+          }}
+        />
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-5 p-2 text-black  ">
         {searchItems.map((item) => (
           <Item
