a2dc62d321eb3c95a68859e08a76eb2028045e9a
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-10-15 19:29:34 +0200

    adj: back to homepage btn as <a> instead of <Link>

--- src/routes/CheckoutSuccess.jsx
+++ src/routes/CheckoutSuccess.jsx
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
 
 export default function CheckoutSuccess() {
+  function refreshPage() {
+    window.location.reload();
+  }
   return (
     <div className="h-screen flex justify-center items-start">
       <div className="flex flex-col gap-2 bg-slate-100 border-slate-600 border-2 rounded-md p-10 mt-20 text-center">
         <h1>Success!</h1>
         <p>Your order has been placed and your cart is cleared.</p>
-        <Link
-          to="/"
+        <a
+          href="/"
           className="bg-teal-500 text-black p-2 w-full rounded-md transition ease-in-out hover:bg-teal-300  duration-300"
         >
           Go back to homepage
-        </Link>
+        </a>
       </div>
     </div>
   );
